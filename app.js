8ae66113e3f9c7f554cc3cb54aadd609da522db2
ASMRSAEED <asmrsaeed@gmail.com>
2023-02-18 00:42:31 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// const allButton = document.getElementsByClassName('button-test');
+// for (const button of allButton) {
+//     button.addEventListener('click', function (e) {
+//         // console.log(e.target.parentNode);
+//         if (e.target.id=='prime'){
+//             document.getElementById('title').style.color = 'red';
+
+//         }else{
+//             document.getElementById('title').style.color = 'green';
+//         }
+
+//     })
+
+// };
+
+// const prime = document.getElementById('prime');
+// console.log(prime);
+
+
+let count = 0;
+// document.getElementById('plus').addEventListener('click', function (e) {
+//     count++
+//     control('count')
+// })
+
+function plus() {
+    count++
+    control('count')
+}
+// document.getElementById('minus').addEventListener('click', function (e) {
+//     count--
+//     control('count')
+// })
+
+function minus() {
+    if (count == 0) {
+        return
+    } else {
+        count--
+        control('count')
+    }
+}
+
+function control(id) {
+    document.getElementById(id).innerText = count;
+}
